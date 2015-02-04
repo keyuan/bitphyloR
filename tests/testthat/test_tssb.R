26c081abf2065ca844242fcc791211332a07aa8e
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-04 17:54:01 +0000

    first test

--- tests/testthat/test_tssb.R
+++ tests/testthat/test_tssb.R
@@ -1,5 +1,8 @@
-
-
-tssb <- TSSB$new(n0, data = matrix(rnorm(50),10000,1))
+tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
+                 dpLambda = 1)
 res <- tssb$GetMixture()
-(1-sum(res$weights) < 0.2)
+
+expect_true((1-sum(res$weight)) < 0.3)
+
+barplot(res$weight)
+print(sum(res$weight))
