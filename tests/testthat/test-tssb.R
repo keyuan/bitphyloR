8cbd45290fc870281053a2940b6f0c2dc197c8f2
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-05 12:13:22 +0000

    rename test file for tssb

--- tests/testthat/test-tssb.R
+++ tests/testthat/test-tssb.R
@@ -3,6 +3,3 @@ tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
 res <- tssb$GetMixture()
 
 expect_true((1-sum(res$weight)) < 0.3)
-
-barplot(res$weight)
-print(sum(res$weight))
