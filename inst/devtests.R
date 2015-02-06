20a1d7d93f31e1c0044483d8092d9bf126cccba5
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-06 17:39:01 +0000

    add tests

--- inst/devtests.R
+++ inst/devtests.R
@@ -1,6 +1,8 @@
 n0 <- Node$new()
-tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
-                 dpLambda = 1)
+tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1/5,
+                 dpLambda = 1/2)
 res <- tssb$GetMixture()
 
 res1 <- tssb$ConvertTssbToIgraph()
+
+plot(res1$g)
