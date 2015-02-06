9dc38a396d6a7c9c0df756b2d1db2bec2cb6f28a
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-06 14:20:09 +0000

    class for batch vb for tssb

---
+++ R/infer_batch_vb.R
@@ -0,0 +1,14 @@
+#' R6 class for inference via Batch variational Bayes for TSSB.
+#'
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords data
+
+TssbBatchVB <- R6Class(
+  classname = "TssbBatchVB",
+  inherit = TSSB
+
+)
