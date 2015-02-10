9bdd35fb288979a7caea9cd2a01dc21aec3da344
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-10 18:27:01 +0000

    test cull tree

--- inst/devtests.R
+++ inst/devtests.R
@@ -1,20 +1,35 @@
 n0 <- Node$new()
 tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1/5,
                  dpLambda = 1/2)
-res <- tssb$GetMixture()
+res1 <- tssb$GetMixture()
 
-res1 <- tssb$ConvertTssbToIgraph()
+sapply(res1$node, function(x) x$GetNumOfLocalData())
 
-TestDescend <- function(root){
-  Descend <- function(root) {
-    if (!is.null(root$sticks)) {
-      root$main <- length(root$children)
-      for (i in 1:length(root$children)) {
-        Descend(root <<- root$children[[i]])
-      }
-    }
-    return(root)
+res2 <- tssb$ConvertTssbToIgraph()
+
+tt <- tssb$root
+
+
+Descend1 <- function(root) {
+  res <- unlist(Map(Descend1, root$children), recursive = F, use.names = F)
+  if (length(res) == 0) {
+    return(list(counts = root$node$GetNumOfLocalData(), root = root))
   }
-  return(Descend(root<<-root))
+  counts <- unlist(res[seq(1, length(res), by = 2)])
+  root$children <- res[seq(2, length(res), by = 2)]
+  keep <- length(TrimZeros(counts, trim = "b"))
+
+  if (keep == 0) {
+    root$sticks <- NULL
+    root$children <- list()
+  } else {
+    root$sticks <- root$sticks[1:keep]
+    root$children <- root$children[1:keep]
+  }
+
+  return(list(counts = sum(counts) + root$node$GetNumOfLocalData(),
+              root = root))
 }
-TestDescend(root<<-tssb$root)
+
+td <- Descend1(tssb$root)
+
