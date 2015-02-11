5ba019007f25e8de137c81ff5402c157a023be3e
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-11 17:15:07 +0000

    new stuffs

--- inst/devtests.R
+++ inst/devtests.R
@@ -1,3 +1,4 @@
+set.seed(9)
 n0 <- Node$new()
 tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
                  dpLambda = 1)
@@ -9,10 +10,29 @@ plot(res2$g, layout = layout.reingold.tilford(res2$g),
 
 tt <- tssb$root
 
-tssb$CullTree()
-
+tssb$CullTree()$CullTree()
 tt1 <- tssb$root
 res3 <- tssb$ConvertTssbToIgraph()
 plot(res3$g,
      layout = layout.reingold.tilford(res3$g),
      vertex.label = get.vertex.attribute(res3$g, name = "size"))
+
+
+set.seed(9)
+tssbMCMC <- TssbMCMC$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
+                 dpLambda = 1)
+
+
+# # Kill test ---------------------------------------------------------------
+# n0 <- Node$new()
+# n1 <- n0$Spawn()
+# n2 <- n0$Spawn()
+# n21 <- n2$Spawn()
+# n22 <- n2$Spawn()
+# n0Children <- n0$GetChildren()
+# n2Children <- n2$GetChildren()
+# n2$Kill()
+# n0ChildrenAfterKill <- n0$GetChildren()
+
+
+
