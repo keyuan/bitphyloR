f7df956b8a3ffb0b57a7ad88275b305f2b9dd13f
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-16 17:18:02 +0000

    test resample sticks

--- inst/devtests.R
+++ inst/devtests.R
@@ -1,41 +1,57 @@
 library(microbenchmark)
 
-set.seed(9)
-n0 <- Node$new()
-tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
-                 dpLambda = 1)
-res1 <- tssb$GetMixture()
-res2 <- tssb$ConvertTssbToIgraph()
+#set.seed(9)
+# n0 <- Node$new()
+# tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
+#                  dpLambda = 1)
+# res1 <- tssb$GetMixture()
+# res2 <- tssb$ConvertTssbToIgraph()
+#
+# plot(res2$g, layout = layout.reingold.tilford(res2$g),
+#      vertex.label = get.vertex.attribute(res2$g, name = "size"),
+#      vertex.size = 15, edge.arrow.size = 0.5)
+#
+# tt <- tssb$root
+#
+# tssb$CullTree()$CullTree()
+# tt1 <- tssb$root
+# res3 <- tssb$ConvertTssbToIgraph()
+# plot(res3$g,
+#      layout = layout.reingold.tilford(res3$g),
+#      vertex.label = get.vertex.attribute(res3$g, name = "size"),
+#      vertex.size = 15, edge.arrow.size = 0.5)
 
-plot(res2$g, layout = layout.reingold.tilford(res2$g),
-     vertex.label = get.vertex.attribute(res2$g, name = "size"),
-     vertex.size = 15, edge.arrow.size = 0.5)
+res1 = c()
+res2 = c()
+res3 = c()
+res4 = c()
+res5 = c()
+res6 = c()
+for (i in 1:1000){
+  print(i)
+  n0 <- Node$new()
+  tssbMCMC <- TssbMCMC$new(n0,
+                           data = matrix(rnorm(50),50,1),
+                           dpAlpha = 1,
+                           dpGamma = 1,
+                           dpLambda = 1)
+  ww <- tssbMCMC$GetMixture()$weight
+  res1 <- c(res1, sum(ww))
+  res2 <- c(res2, length(ww))
 
-tt <- tssb$root
+  tssbMCMC$CullTree()
+  ww <- tssbMCMC$GetMixture()$weight
+  res3 <- c(res3, sum(ww))
+  res4 <- c(res4, length(ww))
 
-tssb$CullTree()$CullTree()
-tt1 <- tssb$root
-res3 <- tssb$ConvertTssbToIgraph()
-plot(res3$g,
-     layout = layout.reingold.tilford(res3$g),
-     vertex.label = get.vertex.attribute(res3$g, name = "size"),
-     vertex.size = 15, edge.arrow.size = 0.5)
+  ww <- tssbMCMC$ResampleSticks()$GetMixture()$weight
+  res5 <- c(res5, sum(ww))
+  res6 <- c(res6, length(ww))
 
-
-set.seed(9)
-tssbMCMC <- TssbMCMC$new(n0,
-                         data = matrix(rnorm(50),50,1),
-                         dpAlpha = 1,
-                         dpGamma = 1,
-                 dpLambda = 1)
-tssbMCMC$CullTree()
-res4 <- tssbMCMC$GetMixture()
-nodes <- res4$node
+}
 
 
 
-# test resample sticks
-originalTree <- tssbMCMC$root
-afterResampleTree <- tssbMCMC$ResampleSticks()$root
-res5 <- tssbMCMC$GetMixture()
+
+
 
