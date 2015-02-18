90db11972e803a1adc992ffe62210a1150c51a21
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-18 01:39:11 +0000

    ResampleSticks bug fixed

--- inst/devtests.R
+++ inst/devtests.R
@@ -21,14 +21,16 @@ library(microbenchmark)
 #      vertex.label = get.vertex.attribute(res3$g, name = "size"),
 #      vertex.size = 15, edge.arrow.size = 0.5)
 
-res1 = c()
-res2 = c()
-res3 = c()
-res4 = c()
-res5 = c()
-res6 = c()
-for (i in 1:1){
-  print(i)
+set.seed(10)
+n = 1000
+res1 = vector("numeric", n)
+res2 = res1
+res3 = res1
+res4 = res1
+res5 = res1
+res6 = res1
+for (i in seq_along(res1)){
+  cat(i)
   n0 <- Node$new()
   tssbMCMC <- TssbMCMC$new(n0,
                            data = matrix(rnorm(50),50,1),
@@ -36,20 +38,29 @@ for (i in 1:1){
                            dpGamma = 1,
                            dpLambda = 1)
   ww <- tssbMCMC$GetMixture()$weight
-  res1 <- c(res1, sum(ww))
-  res2 <- c(res2, length(ww))
-  root1 <- tssbMCMC$root
+  res1[i] <- sum(ww)
+  res2[i] <- length(ww)
+#   root1 <- tssbMCMC$root
+#   g <- tssbMCMC$ConvertTssbToIgraph()$g
+#   plot(g,
+#        layout = layout.reingold.tilford(g),
+#        vertex.label = get.vertex.attribute(g, name = "size"),
+#        vertex.size = 15, edge.arrow.size = 0.5)
 
   tssbMCMC$CullTree()
   ww <- tssbMCMC$GetMixture()$weight
-  res3 <- c(res3, sum(ww))
-  res4 <- c(res4, length(ww))
-  root2 <- tssbMCMC$root
+  res3[i] <- sum(ww)
+  res4[i] <- length(ww)
+#   root2 <- tssbMCMC$root
+#   g <- tssbMCMC$ConvertTssbToIgraph()$g
+#   plot(g,
+#        layout = layout.reingold.tilford(g),
+#        vertex.label = get.vertex.attribute(g, name = "size"),
+#        vertex.size = 15, edge.arrow.size = 0.5)
 
   ww <- tssbMCMC$ResampleSticks()$GetMixture()$weight
-  res5 <- c(res5, sum(ww))
-  res6 <- c(res6, length(ww))
-  root3 <- tssbMCMC$root
+  res5[i] <- sum(ww)
+  res6[i] <- length(ww)
 
 }
 
