b16fbc22286ac33ef0de7cd6bced8e35ad05012d
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-26 17:29:44 +0000

    initialization of normal node

--- R/node_normal.R
+++ R/node_normal.R
@@ -16,5 +16,34 @@ NULL
 #' @method GetChildren
 Normal <- R6::R6Class(
   classname = "Normal",
-  inherit = Node
+  inherit = Node,
+  public = list(
+    initialize = function(parent = NULL, tssb = NULL) {
+      super$new(parent = parent, tssb = tssb)
+      if (is.null(parent)) {
+        private$drift <- drift
+        private$priorDriftScale <- priorDriftScale
+        private$priorDriftDof <-priorDriftDof
+        private$priorSigmaScale <- priorSigmaScale
+        private$priorSigmaDof <- priorSigmaDof
+        self$sigma <- MCMCpack::riwish(v = priorSigmaDof, S = priorSigmaScale)
+        self$params <- mvtnorm::rmvnorm(n = 1,
+                                        mean = initMean,
+                                        sigma = private$drift)
+      } else {
+        self$sigma <- MCMCpack::riwish(v = priorSigmaDof, S = priorSigmaScale)
+        self$params <- mvtnorm::rmvnorm(n = 1,
+                                        mean = parent$params,
+                                        sigma = self$GetDrift())
+      }
+
+    }
+
+
+    ),
+  private = list(
+    drift = NA,
+    priorDriftScale = NA,
+    priorDriftDof = NA
+    )
 )
