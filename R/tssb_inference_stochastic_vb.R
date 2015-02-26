820fc79503ea02c8919bc4aba4d3707eb16b23c5
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-26 14:04:09 +0000

    stochastic vb class

---
+++ R/tssb_inference_stochastic_vb.R
@@ -0,0 +1,16 @@
+#'@include tssb.R
+NULL
+
+#' R6 class for inference via stochastic variational Bayes for TSSB.
+#'
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords data
+
+TssbSVB <- R6::R6Class(
+  classname = "TssbSVB",
+  inherit = TSSB
+)
