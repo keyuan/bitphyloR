290fc566ee2dcbe06d19754925062d33d7b8aa4a
Ke Yuan <ke.yuan.09@gmail.com>
2015-02-26 14:03:11 +0000

    initial normal node

---
+++ R/node_normal.R
@@ -0,0 +1,20 @@
+#'@include node.R
+NULL
+
+#' R6 class for Normal node.
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords Normal node class
+#' @field dataIds: data IDs
+#' @field tssb: A TSSB object
+#' @field children: descentant node objects
+#' @field parent: parent node object
+#' @method initialize
+#' @method GetChildren
+Normal <- R6::R6Class(
+  classname = "Normal",
+  inherit = Node
+)
