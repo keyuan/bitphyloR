fb225e5274613aeab0ff93d55b85d51c7263b556
Ke Yuan <ke.yuan.09@gmail.com>
2015-05-05 16:52:32 +0100

    test resample node params

--- R/node_normal.R
+++ R/node_normal.R
@@ -94,9 +94,18 @@ Normal <- R6::R6Class(
     },
 
     ResampleParams = function() {
-      data <- self$GetData()
+      nodeData <- self$GetData()
       drift <- self$GetDrift()
-      numOfData <- nrow(data)
+      numOfData <- nrow(nodeData)
+      priorSigmaScale <- self$GetPriorSigmaScale()
+      priorSigmaDof <- self$GetPriorSigmaDof()
+      childern <- private$children
+
+      if (numOfData == 0) {
+        dataMean = 0
+      } else {
+        dataMean = colMeans(nodeData)
+      }
 
       if (is.null(private$parent)) {
         parentParams <- self$initMean
@@ -104,23 +113,44 @@ Normal <- R6::R6Class(
         parentParams <- private$parent$params
       }
 
-      #invDrift <- chol2inv(chol(drift))
-
-      childParams <- Reduce(
-        rbind,
-        Map(function(x) {x$params}, root$GetChildren()),
-        c()
+      if (is.null(childern)) {
+        numOfChildren = 0
+        childParamsMean = 0
+      } else {
+        childParams <- Reduce(
+          rbind,
+          Map(function(x) {x$params}, childern),
+          c()
         )
-
-      numOfChildren <- nrow(childParams)
-      childParamsMean <- colMeans(childParams)
-
+        numOfChildren <- nrow(childParams)
+        childParamsMean <- colMeans(childParams)
+      }
 
       # Construct prior for node mean
-      priorParams <- (parentParams + numOfChildren*childParamsMean) / (numOfChildren + 1)
+      priorParamsMean <- (parentParams + numOfChildren*childParamsMean)/(numOfChildren + 1)
       priorParamsCov <- drift / (numOfChildren + 1)
 
+      # Posterior for node mean
+      tmpCov <- priorParamsCov + (1/numOfData) * self$sigma
+      invTmpCov <- chol2inv(chol(tmpCov))
+      postParamsCov <- priorParamsCov + priorParamsCov%*%invTmpCov%*%priorParamsCov
+      postParamsMean <- postParamsCov%*%
+        (chol2inv(chol(priorParamsCov))%*%priorParamsMean +
+           numOfData*chol2inv(chol(self$sigma))%*%dataMean)
 
+      self$params <- rmvnorm(n = 1,
+                            mean = postParamsMean,
+                            sigma = postParamsCov)
+
+      # Posterior for node covarirance
+      if (numOfData > 0) {
+        postSigmaDof <- priorSigmaDof + numOfData
+        ## need fix
+        tmpVec <- nodeData - self$params
+        postSigmaScale <- priorSigmaScale + t(tmpVec) %*% tmpVec
+
+        self$sigma <- riwish(v = postSigmaDof, S = postSigmaScale)
+      }
     },
 
     ResampleHyperParams = function() {
