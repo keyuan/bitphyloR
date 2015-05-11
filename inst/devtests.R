64f8873a1d451c3402c277c2122c550f5f864a53
Ke Yuan <ke.yuan.09@gmail.com>
2015-05-11 17:34:53 +0100

    test resample hypers

--- inst/devtests.R
+++ inst/devtests.R
@@ -29,6 +29,8 @@ tssbMCMC$ResampleStickOrders()
 
 tssbMCMC$CullTree()
 
+tssbMCMC$ResampleHypers()
+
 #   root1 <- tssbMCMC$root
 #   g <- tssbMCMC$ConvertTssbToIgraph()$g
 #   plot(g,
@@ -43,12 +45,18 @@ tssbMCMC$CullTree()
 #        vertex.label = get.vertex.attribute(g, name = "size"),
 #        vertex.size = 15, edge.arrow.size = 0.5)
 
-fx <- function(x) {
-  - 0.5*(x-200)^2/(10)
-}
+# fx <- function(x, y) {
+#   print(y)
+#   - 0.5*(x-0)^2/(1) - 0.5*(y-0)^2/(1)
+# }
+#
+# n = 1000
+# x = array(0, dim = c(n,1))
+# for (i in seq_len(n-1)) {
+#   x[i+1,] = SliceSampler(x[i,], fx, compwise = T, stepOut = T, y = 10)
+# }
 
-n = 1000
-x = array(0, dim = n)
-for (i in seq_len(n-1)) {
-  x[i+1] = SliceSampler(x[i], fx, compwise = F, stepOut = T)
-}
+
+
+
+
