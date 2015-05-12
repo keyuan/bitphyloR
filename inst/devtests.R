241c8208484f15f19a5ecb4dde029ed4deb2c707
Ke Yuan <ke.yuan.09@gmail.ocm>
2015-05-12 23:55:05 +0100

    test resample node hypers

--- inst/devtests.R
+++ inst/devtests.R
@@ -3,7 +3,7 @@ library(microbenchmark)
 set.seed(10)
 
 m = 100
-testData = rbind( matrix(0.5*rnorm(m)+20,m,1),  matrix(0.5*rnorm(m)+10,m,1))
+testData = rbind( matrix(0.1*rnorm(m)+20,m,1),  matrix(rnorm(m)+10,m,1))
 n0 <- Normal$new()
 tssbMCMC <- TssbMCMC$new(n0,
                          data = testData,
@@ -12,8 +12,8 @@ tssbMCMC <- TssbMCMC$new(n0,
                          dpLambda = 1)
 
 #
-n = 1000
-
+n = 1
+gg = vector(mode = "list", length = n)
 ll = array(0, dim = n)
 pp = array(0, dim = c(n, 2*m))
 ss = array(0, dim = c(n, 2*m))
@@ -33,6 +33,8 @@ for (i in seq_len(n)) {
 
   tssbMCMC$ResampleNodeParameters()
 
+  n0$ResampleHyperParams()
+
   tssbMCMC$ResampleSticks()
 
   tssbMCMC$ResampleStickOrders()
@@ -47,14 +49,19 @@ for (i in seq_len(n)) {
   dpl[i] <- tssbMCMC$dpLambda
   dpg[i] <- tssbMCMC$dpGamma
 
+  gg[[i]] <- tssbMCMC$ConvertTssbToIgraph()$g
 }
 
 
 g <- tssbMCMC$ConvertTssbToIgraph()$g
-plot(g,
-     layout = layout.reingold.tilford(g),
-     vertex.label = get.vertex.attribute(g, name = "size"),
-     vertex.size = 15, edge.arrow.size = 0.5)
+
+for (i in 1:n) {
+  plot(gg[[i]],
+       layout = layout.reingold.tilford(gg[[i]]),
+       vertex.label = get.vertex.attribute(gg[[i]], name = "size"),
+       vertex.size = 15, edge.arrow.size = 0.5)
+
+}
 
 ## tssbMCMC$ResampleNodeParameters()
 #   root2 <- tssbMCMC$root
