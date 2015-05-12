e97dd63e1ac9f50a308933b40a8a742c2f6dacdc
Ke Yuan <ke.yuan.09@gmail.com>
2015-05-12 15:53:11 +0100

    test pipline

--- inst/devtests.R
+++ inst/devtests.R
@@ -2,7 +2,8 @@ library(microbenchmark)
 
 set.seed(10)
 
-testData = matrix(rnorm(50),50,1)
+m = 100
+testData = rbind( matrix(0.5*rnorm(m)+20,m,1),  matrix(0.5*rnorm(m)+10,m,1))
 n0 <- Normal$new()
 tssbMCMC <- TssbMCMC$new(n0,
                          data = testData,
@@ -10,47 +11,52 @@ tssbMCMC <- TssbMCMC$new(n0,
                          dpGamma = 1,
                          dpLambda = 1)
 
-
-n = 100
+#
+n = 1000
 
 ll = array(0, dim = n)
+pp = array(0, dim = c(n, 2*m))
+ss = array(0, dim = c(n, 2*m))
+dpa = array(0, dim = n)
+dpl = array(0, dim = n)
+dpg = array(0, dim = n)
 
 for (i in seq_len(n)) {
 
-  print(i)
+  if (i%%50== 0) {
+    print(i)
+  }
 
   tssbMCMC$ResampleAssignments()
-
 
   tssbMCMC$CullTree()
 
   tssbMCMC$ResampleNodeParameters()
 
-
   tssbMCMC$ResampleSticks()
-
 
   tssbMCMC$ResampleStickOrders()
 
-
-  tssbMCMC$CullTree()
-
-
-  #tssbMCMC$ResampleHypers()
+  tssbMCMC$ResampleHypers()
 
   ll[i] <- tssbMCMC$GetLogMarginalDataLikelihood()
+  pp[i, ] <- sapply(tssbMCMC$assignments, function(x) x$params)
+  ss[i, ] <- sapply(tssbMCMC$assignments, function(x) x$sigma)
+
+  dpa[i] <- tssbMCMC$dpAlpha
+  dpl[i] <- tssbMCMC$dpLambda
+  dpg[i] <- tssbMCMC$dpGamma
 
 }
 
 
+g <- tssbMCMC$ConvertTssbToIgraph()$g
+plot(g,
+     layout = layout.reingold.tilford(g),
+     vertex.label = get.vertex.attribute(g, name = "size"),
+     vertex.size = 15, edge.arrow.size = 0.5)
 
-#   root1 <- tssbMCMC$root
-#   g <- tssbMCMC$ConvertTssbToIgraph()$g
-#   plot(g,
-#        layout = layout.reingold.tilford(g),
-#        vertex.label = get.vertex.attribute(g, name = "size"),
-#        vertex.size = 15, edge.arrow.size = 0.5)
-
+## tssbMCMC$ResampleNodeParameters()
 #   root2 <- tssbMCMC$root
 #   g <- tssbMCMC$ConvertTssbToIgraph()$g
 #   plot(g,
