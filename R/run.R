a1dfd52c319b3a7f79bf704d7981c266dfe98bf1
Ke Yuan <ke.yuan.09@gmail.com>
2015-05-14 18:08:30 +0100

    tested run normal

--- R/run.R
+++ R/run.R
@@ -1,53 +1,78 @@
 #' Fit a tssb with normal node with data via mcmc
-#' @param data a data matrix
+#' @param data a data matrix or list
 #' @param numOfMCMC mcmc iterations
+#' @param burnIn burn-in number
+#' @param dims dimension of data
+#' @param tssb a use supplied TssbMCMC object
+#' @param ... additional parameters to node class generator
 #' @return A list of traces
 #' @export
 
-RunNormal <- function (data, numOfMCMC, tssb = NULL) {
+RunNormal <- function (data,
+                       numOfMCMC = 1000,
+                       burnIn = 0,
+                       dims = ncol(data),
+                       tssb = NULL,
+                       ...) {
 
-  priorSigmaScale <- var(data)
-  priorDriftScale <- priorSigmaScale
-  rootNode <- Normal(priorSigmaScale = priorSigmaScale, priorDriftScale = priorDriftScale)
-  tssb <- TssbMCMC$new(rootNode, data = data)
-  dims <- ncol(data)
-  numOfData <- nrow(data)
+  if (is.list(data)) {
+    numOfData <- length(data)
+  } else if (is.matrix(data)) {
+    numOfData <- nrow(data)
+  } else {
+    stop("data has to be list or matrix")
+  }
 
   gg = vector(mode = "list", length = numOfMCMC)
   ll = array(0, dim = numOfMCMC)
-  pp = array(0, dim = c(numOfMCMC, numOfData))
-  ss = array(0, dim = c(numOfMCMC, numOfData))
+  pp = array(0, dim = c(m, dims, numOfMCMC))
+  ss = array(0, dim = c(m*dims, dims, numOfMCMC))
   dpa = array(0, dim = numOfMCMC)
   dpl = array(0, dim = numOfMCMC)
   dpg = array(0, dim = numOfMCMC)
-  dd = array(0, dim = numOfMCMC)
+  dd = array(0, dim = c(dims, dims, numOfMCMC))
 
+  if (is.null(tssb)) {
+    root <- Normal$new(...)
+    tssb <- TssbMCMC$new(root, data = data)
+  } else {
+    root <- tssb$root$node
+  }
 
-  for (n in seq_len(numOfMCMC)) {
-    tssbMCMC$ResampleAssignments()
+  for (i in burnIn:numOfMCMC) {
 
-    tssbMCMC$CullTree()
+    tssb$ResampleAssignments()
 
-    tssbMCMC$ResampleNodeParameters()
+    tssb$CullTree()
 
-    rootNode$ResampleHyperParams()
+    tssb$ResampleNodeParameters()
 
-    tssbMCMC$ResampleSticks()
+    root$ResampleHyperParams()
 
-    tssbMCMC$ResampleStickOrders()
+    tssb$ResampleSticks()
 
-    tssbMCMC$ResampleHypers()
+    tssb$ResampleStickOrders()
 
-    ll[i] <- tssbMCMC$GetLogMarginalDataLikelihood()
-    pp[i, ] <- sapply(tssbMCMC$assignments, function(x) x$params)
-    ss[i, ] <- sapply(tssbMCMC$assignments, function(x) x$sigma)
+    tssb$ResampleHypers()
 
-    dpa[i] <- tssbMCMC$dpAlpha
-    dpl[i] <- tssbMCMC$dpLambda
-    dpg[i] <- tssbMCMC$dpGamma
-    dd[i] <- n0$GetDrift()
-    gg[[i]] <- tssbMCMC$ConvertTssbToIgraph()$g
+    res <- tssb$GetLogMarginalDataLikelihood()
 
+     if (i >0) {
+      ll[i] <- res$ll
+      pp[,,i] <- Reduce(rbind, Map(function(x) x$params, tssb$assignments))
+      ss[,,i] <- Reduce(rbind, Map(function(x) x$sigma, tssb$assignments))
+      dpa[i] <- tssb$dpAlpha
+      dpl[i] <- tssb$dpLambda
+      dpg[i] <- tssb$dpGamma
+      dd[,,i] <- n0$GetDrift()
+      gg[[i]] <- tssb$ConvertTssbToIgraph()$g
+    }
+
+    if (i%%100 == 0) {
+      cat("iteration:", i,  "average datum marginal likelihood:", res$ll/numOfData,
+          "nodes:", length(res$ww), "big nodes:",  length(res$ww > 0.01),
+          "\n")
+    }
   }
 
   return(list(likelihood = ll,
@@ -56,5 +81,6 @@ RunNormal <- function (data, numOfMCMC, tssb = NULL) {
               dpAlpha = dpa,
               dpLambda= dpl,
               dpGamma = dpg,
-              igraph = gg))
+              igraph = gg,
+              drift = dd))
 }
