9d64cbd20c93754e7386c8e5b195ed54b13ae810
Ke Yuan <ke.yuan.09@gmail.com>
2015-05-20 14:45:29 +0100

    add beta binomial functions

--- R/util.R
+++ R/util.R
@@ -128,6 +128,27 @@ BoundBeta <- function(...){
     )
 }
 
+
+dbb <- function(x, N, u, v) {
+  beta(x+u, N-x+v)/beta(u,v)*choose(N,x)
+}
+
+pbb <- function(q, N, u, v) {
+  sapply(q, function(xx) sum(dbb(0:xx, N, u, v)))
+}
+
+qbb <- function(p, N, u, v) {
+  pp <- cumsum(dbb(0:N, N, u, v))
+  sapply(p, function(x) sum(pp < x))
+}
+
+rbb <- function(n, N, u, v) {
+  p <- rbeta(n, u, v)
+  rbinom(n, N, p)
+}
+
+
+
 repmat <- function(x, n, m){
   x <- as.matrix(x)
   return(kronecker(matrix(1,n,m),x))
