8b62de1b47cb6e7b5e53d62b395dbb960b884b1b
Ke Yuan <ke.yuan.09@gmail.com>
2015-07-27 22:59:59 +0100

    minor tweaks

--- tests/testthat/test-tssb.R
+++ tests/testthat/test-tssb.R
@@ -1,6 +1,18 @@
-n0 <- Node$new()
-tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
-                 dpLambda = 1)
-res <- tssb$GetMixture()
+test_that("classname contains 'TSSB'", {
+  n0 <- Node$new()
+  tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
+                   dpLambda = 1)
+  expect_that("TSSB" %in% class(tssb), is_true())
+})
 
-expect_true((1-sum(res$weight)) < 0.3)
+test_that("Sum of sticks needs to be close to one", {
+  n0 <- Node$new()
+  tssb <- TSSB$new(n0, data = matrix(rnorm(50),50,1), dpAlpha = 1, dpGamma = 1,
+                   dpLambda = 1)
+  res <- tssb$GetMixture()
+  expect_true((1-sum(res$weight)) < 0.3)
+})
+
+
+
+
